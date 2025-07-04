5cdb73de94a6c90053650e597dd11848d63c15e7
AGT-tech <abil.teshome@maine.edu>
2025-07-04 11:04:05 -0400

    Add WeatherApp class with basic weather API setup

--- core/api.py
+++ core/api.py
@@ -1,4 +1,12 @@
 import requests
 
+class WeatherAPI:
+    """Basic weahter application with core functionality"""
 
+    def __init__(self, api_key: str):
+        self.api_key = api_key
+        self.base_url = "http://api.openweathermap.org/data/2.5/weather"
+        self.timeout = 10
+        
+    
 
